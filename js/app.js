ad3359c3b6131d45d7d2ca2c32254415b4e3d9b0
johndotwoo <johndotwoo@hotmail.com>
2014-05-15 22:34:36 -0400

    fizzbuzz refactor
    added basic CSS styling and input validation via JS and jQuery

--- js/app.js
+++ js/app.js
@@ -1,23 +1,37 @@
 $(document).ready(function() {
-	$(document).on('click','.startButton',function() {
 
-		var i = 1;
-		for(i; i < 101; i++) {
-
-			if((i % 3 == 0) && (i % 5 == 0)) {
-				$('.fizzBuzzCounts').append('<p>fizz buzz</p>');
-			}
-			else if(i % 3 == 0) {
-				$('.fizzBuzzCounts').append('<p>fizz</p>');
-			}
-
-			else if(i % 5 == 0) {
-				$('.fizzBuzzCounts').append('<p>buzz</p>');
-			}
-
-			else {
-				$('.fizzBuzzCounts').append('<p>' + i + '</p>');
-			}
-		}
+	$('#fizzBuzzContainer').on('click','.startButton',function() {
+		entryFromUser = +$('#formEntryFromUser').val();
+		$('#formEntryFromUser').val('');		// clear form entry
+		validateThenFizzBuzz(entryFromUser);	// validate input, if true execute fizzbuzz
 	});
+
 });
+
+function validateThenFizzBuzz(form) {
+	if(form == "" || form ==null || form < 1 || form > 999) {
+		alert("Please enter a number from 1 to 999");
+		return false;
+	}
+	else {
+		$('.fizzBuzzCounts').empty();			// dump previous fizzBuzz content
+		fizzBuzzFxn(entryFromUser);				// run fizz buzz function
+	}
+}
+
+function fizzBuzzFxn(number) {
+	for(var i = 1; i <= number; i++) {
+		if((i % 3 == 0) && (i % 5 == 0)) {
+			$('.fizzBuzzCounts').append('<p class="fizzBuzz">fizz buzz</p>');
+		}
+		else if(i % 3 == 0) {
+			$('.fizzBuzzCounts').append('<p class="fizz">fizz</p>');
+		}
+		else if(i % 5 == 0) {
+			$('.fizzBuzzCounts').append('<p class="buzz">buzz</p>');
+		}
+		else {
+			$('.fizzBuzzCounts').append('<p>' + i + '</p>');
+		}
+	}
+}
